a24cdf3f5bea50ab65d8b98b12b8740454e2de69
Tristan <tsilvas0113@gmail.com>
2022-07-01 08:13:51 -0500

    Allow form text to be added to meme image

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -20,22 +20,43 @@ function Meme () {
             }
         })
     }
+
+    function handleChange(event) {
+        setMeme(prevData => {
+            return {
+                ...prevData,
+                [event.target.name]: event.target.value
+            }
+        })
+    }
+
+
     return (
-        <main className="meme">
+        <main>
             <div className="form">
                 <input 
                     type='text'
                     placeholder="Top text"
-                    className="form-input" 
+                    className="form-input"
+                    name='topText'
+                    value={meme.topText}
+                    onChange={handleChange} 
                 />
                 <input 
                     type='text'
                     placeholder="Bottom text"
-                    className="form-input" 
+                    className="form-input"
+                    name='bottomText'
+                    value={meme.bottomText}
+                    onChange={handleChange} 
                 />
                 <button onClick={getMemeImage}>Get a new meme image 🖼</button>
             </div>
-            <img className="meme-image" src={meme.randomImage} alt='' />
+            <div className="meme">
+                <img className="meme-image" src={meme.randomImage} alt='' />
+                <h2 className="meme--text top">{meme.topText}</h2>
+                <h2 className="meme--text bottom">{meme.bottomText}</h2>
+            </div>
         </main>
     )
 }
